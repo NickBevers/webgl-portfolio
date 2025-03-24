1d117d835b1085c79d98c92f3ae48dce5ecbb5d2
NickBevers <nick.bevers9@gmail.com>
2025-03-24 21:15:36 +0100

    feat: adds mobile design for organic growth

--- src/components/organicGrowth/OrganicGrowthPlane.tsx
+++ src/components/organicGrowth/OrganicGrowthPlane.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Canvas, useFrame, useThree } from '@react-three/fiber';
 import { PerspectiveCamera, Plane } from '@react-three/drei';
 import styles from './organicGrowth.module.css';
@@ -49,6 +49,18 @@ const Scene: React.FC = () => {
 }
 
 const OrganicGrowthPlane: React.FC = () => {
+  const [isScrolled, setIsScrolled] = useState<boolean>(false);
+
+  const handleScroll = () => {
+    if (window.scrollY > 50) setIsScrolled(true);
+    else setIsScrolled(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
     <section className={styles.plane}>
       <Canvas >
@@ -60,6 +72,9 @@ const OrganicGrowthPlane: React.FC = () => {
       <div className={styles.name}>
         <span className={`${styles.firstname} ${styles.active}`}>Nick</span>
         <span className={`${styles.lastname} ${styles.active}`}>Bevers</span>
+      </div>
+      <div className={`${styles.scrollIcon} ${isScrolled ? styles.hidden : ''}`}>
+        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill={'#ECE7DF'} viewBox="0 0 256 256"><path d="M144,16H112A64.07,64.07,0,0,0,48,80v96a64.07,64.07,0,0,0,64,64h32a64.07,64.07,0,0,0,64-64V80A64.07,64.07,0,0,0,144,16Zm48,160a48.05,48.05,0,0,1-48,48H112a48.05,48.05,0,0,1-48-48V80a48.05,48.05,0,0,1,48-48h32a48.05,48.05,0,0,1,48,48ZM136,64v48a8,8,0,0,1-16,0V64a8,8,0,0,1,16,0Z"></path></svg>
       </div>
     </section>
   );
